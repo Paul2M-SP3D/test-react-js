40204e0201cbafc5fc6f8723890e9bddd7ee7b66
Paul de Misouard <paul.de-misouard@sp3d.co>
2020-01-20 09:54:48 +0800

    initial commit

---
+++ build/precache-manifest.dd89fbdd161e947d08e50593fd4a0e43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d9651d93ee313b23db61e481ca7539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c142d138da3b667734f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "53a5babe56a24189de84",
+    "url": "/static/css/main.2e19147c.chunk.css"
+  },
+  {
+    "revision": "8c142d138da3b667734f",
+    "url": "/static/js/2.9e6fe8ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9e6fe8ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53a5babe56a24189de84",
+    "url": "/static/js/main.ae6a76c5.chunk.js"
+  },
+  {
+    "revision": "89b89fff14a191b19919",
+    "url": "/static/js/runtime-main.1bdcfc2d.js"
+  },
+  {
+    "revision": "b33d6b98e2b3f12c1c822999ccf01173",
+    "url": "/static/media/background.b33d6b98.jpg"
+  }
+]);
